9dc670f7205e2ba425aba2886faf5cc9a0a91828
Lorenzo Mazzarotto <deinitializer@icloud.com>
2023-03-07 04:35:16 -0300

    Initial Commit

---
+++ Feedback/FeedbackApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeedbackApp.swift
+//  Feedback
+//
+//  Created by Lorenzo Mazzarotto on 07/03/23.
+//
+
+import SwiftUI
+
+@main
+struct FeedbackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
